ee4e0a4142fab640204dccaba22dce537663cb3e
Perspicacious <johnayomide920@gmail.com>
2025-02-04 02:26:58 +0100

    fixed nav bug

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -43,7 +43,7 @@ const Navbar = () => {
         </button>
 
          {/* Sign Up Button */}
-         <a href="/signup" className="bg-neon text-white px-4 py-2 rounded-lg hover:bg-purple dark:bg-text-black transition-colors duration-300">
+         <a href="/signup" className="bg-neon text-white  dark:text-black px-4 py-2 rounded-lg hover:bg-purple dark:bg-text-black transition-colors duration-300">
             Get Started
           </a>
 
@@ -78,7 +78,7 @@ const Navbar = () => {
             )}
           </button>
           {/* Sign Up Button */}
-          <a href="/signup" className="bg-indigo-800 text-white px-4 py-2 rounded-lg hover:bg-blue-900 transition-colors duration-300">
+          <a href="/signup" className="bg-neon text-white px-4 py-2 rounded-lg hover:bg-blue-900 transition-colors duration-300">
             Sign Up
           </a>
         </div>
